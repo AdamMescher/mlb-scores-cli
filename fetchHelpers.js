7e1d72e4191585d71f077d733f7ebe2139394b15
Adam Mescher <admescher@gmail.com>
2018-04-10 16:59:11 -0400

    Deleted fetchHelpers.js

--- fetchHelpers.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-const fetchAllTeamIds = () => {
-    fetch(`http://statsapi.mlb.com/api/v1/teams`)
-        .then(res => res.json())
-        .then(res => res.teams.filter(team => team.sport.id === 1).map(team => team.id))
-        .catch(error => console.error(error));
-}
